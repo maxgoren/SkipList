95271cf2f3dc22c86216c902b12f88ec910691c6
Max Goren <65133477+maxgoren@users.noreply.github.com>
2021-09-22 08:00:58 -0400

    Add files via upload

---
+++ skiplist.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <time.h>
+#define MAX_LEVEL 6
+
+struct node {
+    int data;
+    struct node *foward[MAX_LEVEL];
+};
+
+struct SkipList {
+     struct node *head[MAX_LEVEL];
+     struct node *z;
+     int size;
+};
+
+struct node* newNode(int data)
+{
+    struct node *n = malloc(sizeof(struct node));
+    n->data = data;
+    int i;
+    for (i = 0; i < MAX_LEVEL; i++)
+      n->foward[i] = NULL;
+    return n;
+}
+
+struct SkipList* newSkipList()
+{
+    struct SkipList* n = malloc(sizeof(struct SkipList));
+    n->z = newNode(INT_MAX);
+    int i;
+    for (i = 0; i < MAX_LEVEL; i++) 
+    {
+        n->head[i] = newNode(INT_MIN);
+        n->z->foward[i] = n->z;
+        n->head[i]->foward[i] = n->z;  
+    }
+    return n;
+}
+
+unsigned int coinFlip()
+{
+    return 1 - rand() % 2 - 1;
+}
+
+unsigned int randLevel()
+{
+    unsigned int n = 0;
+    while (coinFlip()) n++;
+    return n;
+}
+
+void skipPush(struct SkipList **n, int data)
+{
+    printf("insert called on %d\n", data);
+    struct node *x = newNode(data);
+    int i, lev = randLevel();
+    if (lev > MAX_LEVEL)
+      lev = MAX_LEVEL-1;
+    for (i = 0; i <= lev; i++)
+    {
+        struct node *itr = (*n)->head[i];
+        while (itr->foward[i] != (*n)->z && itr->foward[i]->data < data) {
+            itr = itr->foward[i];
+        }
+        x->foward[i] = itr->foward[i];
+        itr->foward[i] = x;
+        printf("%d inserted at level %d\n", data, i);
+    }
+}
+
+void skipShow(struct SkipList *x)
+{
+    int i;
+    for (i = MAX_LEVEL - 1; i > -1; i--)
+    {
+        if (x->head[i]->foward[i] != x->z)
+        {
+        printf("Level %d: ", i);
+        for (struct node *itr = x->head[i]->foward[i]; itr != x->z; itr = itr->foward[i])
+          printf("%d ", itr->data);
+        printf("\n");
+        } else {
+            printf("Level %d: (empty)\n", i);
+        }
+    }
+}
+
+unsigned int skipSearch(struct SkipList *n)
+{
+   int i;
+   for (i = MAX_LEVEL - 1; i > -1; i--)
+   {
+       
+   }
+}
+
+int main()
+{
+    srand(time(0));
+    int q, i;
+    struct SkipList *mySkip = newSkipList();
+    for (i = 0; i < 13; i++)
+    {
+      q = rand() % (45-1) - 1;
+      printf("random number %d: %d\n", i, q);
+      skipPush(&mySkip, q);
+    }
+    skipShow(mySkip);
+    return 0;
+}
